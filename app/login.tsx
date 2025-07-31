057344cdc4d93d94d97f0a3b4e1d977381ce3f1e
Scott Kirkland <srkirkland@gmail.com>
2025-07-31 14:42:02 -0700

    we got something going

--- app/login.tsx
+++ app/login.tsx
@@ -1,31 +1,65 @@
 import { useAuth } from "@/components/context/AuthContext";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { Redirect, router } from "expo-router";
-import { useState } from "react";
-import { Alert, StyleSheet, TouchableOpacity } from "react-native";
+import { authConfig } from "@/lib/auth";
+import {
+  ResponseType,
+  exchangeCodeAsync,
+  useAuthRequest,
+} from "expo-auth-session";
+import { Redirect } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession(); // needed to close the auth popup
 
 export default function LoginScreen() {
   const [isLoading, setIsLoading] = useState(false);
   const { isLoggedIn, login } = useAuth();
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: authConfig.clientId,
+      redirectUri: authConfig.redirectUri,
+      scopes: ["openid", "profile", "email", "offline_access"],
+      extraParams: {
+        domain_hint: "ucdavis.edu", // Optional: pre-fill the username field with this domain
+      },
+      responseType: ResponseType.Code,
+    },
+    authConfig.discovery
+  );
+
+  useEffect(() => {
+    console.log("Auth response:", response);
+    if (response?.type === "success" && isLoggedIn === false) {
+      const { code } = response.params;
+
+      exchangeCodeAsync(
+        {
+          clientId: authConfig.clientId,
+          code,
+          extraParams: { code_verifier: request?.codeVerifier || "" },
+          redirectUri: authConfig.redirectUri,
+        },
+        authConfig.discovery
+      )
+        .then((tokenResponse) => {
+          console.log("Token response:", tokenResponse);
+          login(tokenResponse);
+        })
+        .catch((error) => {
+          console.error("Error exchanging code for token:", error);
+        });
+    }
+  }, [isLoggedIn, login, request?.codeVerifier, response]);
 
   console.log("LoginScreen rendered");
 
   if (isLoggedIn) {
     return <Redirect href="/" />;
   }
-
-  const handleLogin = async () => {
-    setIsLoading(true);
-    try {
-      login("dummy_token");
-      router.replace("/");
-    } catch {
-      Alert.alert("Error", "Login failed");
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   return (
     <ThemedView style={styles.container}>
@@ -35,7 +69,9 @@ export default function LoginScreen() {
 
       <TouchableOpacity
         style={[styles.button, isLoading && styles.buttonDisabled]}
-        onPress={handleLogin}
+        onPress={() => {
+          promptAsync();
+        }}
         disabled={isLoading}
       >
         <ThemedText style={styles.buttonText}>
--- components/context/AuthContext.tsx
+++ components/context/AuthContext.tsx
@@ -1,9 +1,14 @@
-import { isAuthenticated, setAuthTokenAsync } from "@/lib/auth";
+import {
+  deleteAuthTokensAsync,
+  isAuthenticated,
+  setAuthTokenAsync,
+} from "@/lib/auth";
+import { TokenResponse } from "expo-auth-session";
 import React, { createContext, useContext, useState } from "react";
 
 interface AuthContextType {
   isLoggedIn: boolean;
-  login: (token: string) => void;
+  login: (tokenResponse: TokenResponse) => void;
   logout: () => void;
   isLoading: boolean;
 }
@@ -14,15 +19,15 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isLoggedIn, setIsLoggedIn] = useState(isAuthenticated());
   const [isLoading, setIsLoading] = useState(true); // TODO: if we need it while loading auth state
 
-  const login = (token: string) => {
-    setAuthTokenAsync(token).then(() => {
+  const login = (tokenResponse: TokenResponse) => {
+    setAuthTokenAsync(tokenResponse).then(() => {
       setIsLoggedIn(true);
       setIsLoading(false);
     });
   };
 
   const logout = () => {
-    setAuthTokenAsync(null).then(() => {
+    deleteAuthTokensAsync().then(() => {
       setIsLoading(false);
       setIsLoggedIn(false);
     });
