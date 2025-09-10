4c0e1494c08acbebb394f49ff5baf546eeb4ac17
Scott Kirkland <srkirkland@gmail.com>
2025-09-10 15:42:01 -0700

    recent uses same project list now too

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,35 @@
 import { useAuth } from "@/components/context/AuthContext";
-import { ProjectCard } from "@/components/ui/ProjectCard";
+import { ProjectsList } from "@/components/projects/ProjectsList";
 import { TeamChooser } from "@/components/ui/TeamChooser";
+import { Project } from "@/lib/project";
 import { useRecentProjects } from "@/services/queries/projects";
-import { ScrollView, View } from "react-native";
+import { router } from "expo-router";
+import { ScrollView } from "react-native";
 
 export default function RecentProjectsScreen() {
   const { authInfo } = useAuth();
   const { data: recentProjects } = useRecentProjects(authInfo);
+
+  const handleProjectPress = (project: Project) => {
+    router.push({
+      pathname: "/addExpenses",
+      params: {
+        projectId: project.name,
+        piName: project.piName,
+      },
+    });
+  };
+
   return (
     <ScrollView stickyHeaderIndices={[0]} className="flex-1 flex-col">
       {/* Sticky header */}
       <TeamChooser onClose={() => console.log("Team chooser closed")} />
 
-      {/* Scrollable content */}
-      <View className="p-4">
-        {recentProjects?.map((project) => (
-          <ProjectCard
-            key={project.id}
-            id={project.id}
-            projectId={project.id}
-            piName={project.piName}
-            onEdit={() => console.log("edit pressed")}
-          />
-        ))}
-      </View>
+      <ProjectsList
+        projects={recentProjects || []}
+        queryKey={["projects", authInfo?.team, "recent"]}
+        onProjectPress={handleProjectPress}
+      />
     </ScrollView>
   );
 }
