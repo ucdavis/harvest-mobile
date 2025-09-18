d044e1f094b2e377a571988f179f9d25f417907c
Scott Kirkland <srkirkland@gmail.com>
2025-09-18 14:42:44 -0700

    sync expense queue every time app opens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
-import {
-  DefaultTheme,
-  ThemeProvider
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
+import { AutoSyncManager } from "@/components/AutoSyncManager";
 import { AuthProvider } from "@/components/context/AuthContext";
 import { ExpenseProvider } from "@/components/context/ExpenseContext";
 import { QueryContext } from "@/components/context/QueryContext";
@@ -77,6 +75,7 @@ export default function RootLayout() {
     <QueryContext>
       <AuthProvider>
         <ExpenseProvider>
+          <AutoSyncManager />
           <RootLayoutNav />
         </ExpenseProvider>
       </AuthProvider>
