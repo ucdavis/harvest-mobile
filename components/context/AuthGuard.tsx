f2bdbfe5f61bb286e78a2d3086dabc7fbd75d32f
aristachauhan <aristachauhan@gmail.com>
2025-10-09 14:11:46 -0700

    updated to useSegments

--- components/context/AuthGuard.tsx
+++ components/context/AuthGuard.tsx
@@ -1,11 +1,16 @@
 import { useAuth } from "@/components/context/AuthContext";
-import { usePathname, Redirect } from "expo-router";
+import { Redirect, useSegments } from "expo-router";
+import React from "react";
 
 export function AuthGuard({ children }: { children: React.ReactNode }) {
-  const { isLoggedIn} = useAuth();
-  const pathname = usePathname();
+  const { isLoggedIn } = useAuth();
+  const segments = useSegments() as string[];
 
-  if (!isLoggedIn && !["/login", "/applink", "/not-found"].includes(pathname)) {
+  if (isLoggedIn === null || segments.length === 0) {
+    return null; 
+  }
+
+  if (!isLoggedIn && !["login", "applink", "+not-found"].includes(segments[0])) {
     return <Redirect href="/login" />;
   }
 
