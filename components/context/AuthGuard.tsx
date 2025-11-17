ff21fc99fc4a2adccdf60c80a222650d1605c309
Scott Kirkland <srkirkland@gmail.com>
2025-11-17 12:16:21 -0800

    wait until auth status has loaded before kicking user out

--- components/context/AuthGuard.tsx
+++ components/context/AuthGuard.tsx
@@ -3,14 +3,17 @@ import { Redirect, useSegments } from "expo-router";
 import React from "react";
 
 export function AuthGuard({ children }: { children: React.ReactNode }) {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isLoading } = useAuth();
   const segments = useSegments() as string[];
 
-  if (isLoggedIn === null || segments.length === 0) {
-    return null; 
+  if (isLoading || isLoggedIn === null || segments.length === 0) {
+    return null;
   }
 
-  if (!isLoggedIn && !["login", "applink", "+not-found"].includes(segments[0])) {
+  if (
+    !isLoggedIn &&
+    !["login", "applink", "+not-found"].includes(segments[0])
+  ) {
     return <Redirect href="/login" />;
   }
 
